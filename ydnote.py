ee87d0a343333753771d122ac0e07923be9f6337
rigensen <mr.right.0005@gmail.com>
2020-11-20 22:29:14 +0800

    fix error

--- ydnote.py
+++ ydnote.py
@@ -32,31 +32,23 @@ class YoudaoNoteSession(requests.Session):
             self.cstk = cookies_dict['YNOTE_CSTK']
 	except IOError:
             self.cstk = '0'
-            print('no cookies')
 
     def login(self, username, password):
         self.get('https://note.youdao.com/web/')
         self.headers['Referer'] = 'https://note.youdao.com/web/'
         resp = self.get('https://note.youdao.com/signIn/index.html?&callback=https%3A%2F%2Fnote.youdao.com%2Fweb%2F&from=web')
-        print('signIn status: ' + str(resp.status_code))
         self.headers['Referer'] = 'https://note.youdao.com/signIn/index.html?&callback=https%3A%2F%2Fnote.youdao.com%2Fweb%2F&from=web'
         resp = self.get('https://note.youdao.com/login/acc/pe/getsess?product=YNOTE&_=' + timestamp())
-        print('getsess status: ' + str(resp.status_code))
         resp = self.get('https://note.youdao.com/auth/cq.json?app=web&_=' + timestamp())
-        print('cq.json status : ' + str(resp.status_code))
         resp = self.get('https://note.youdao.com/auth/urs/login.json?app=web&_=' + timestamp())
-        print('login.json status: ' + str(resp.status_code))
         data = {
             "username": username,
             "password": hashlib.md5(password).hexdigest()
         }
         resp = self.post('https://note.youdao.com/login/acc/urs/verify/check?app=web&product=YNOTE&tp=urstoken&cf=6&fr=1&systemName=&deviceType=&ru=https%3A%2F%2Fnote.youdao.com%2FsignIn%2F%2FloginCallback.html&er=https%3A%2F%2Fnote.youdao.com%2FsignIn%2F%2FloginCallback.html&vcode=&systemName=&deviceType=&timestamp=' + timestamp(), data=data, allow_redirects=True)
-        print('login resp: ' + str(resp.status_code))
         resp = self.get('https://note.youdao.com/yws/mapi/user?method=get&multilevelEnable=true&_=' + timestamp())
-        print('multilevelEnable: ' + str(resp.status_code))
         cookies_dict = requests.utils.dict_from_cookiejar(self.cookies)
         self.cstk = cookies_dict['YNOTE_CSTK']
-        print('cstk: ' + self.cstk)
         self.cookies.save('cookies.txt', ignore_discard=True, ignore_expires=True)
 
     def search(self, keyword):
@@ -65,15 +57,12 @@ class YoudaoNoteSession(requests.Session):
         resp = self.post( url, data=data, allow_redirects=True)
         jsons = json.loads(resp.content)
         if resp.status_code != 200:
-            #print("url: " + url)
-            print("webSearch status " + str(resp.status_code))
             return None
         else:
             return jsons['entries']
 
     def getNoteDocx(self, id, saveDir):
         url = 'https://note.youdao.com/ydoc/api/personal/doc?method=download-docx&fileId=%s&cstk=%s&keyfrom=web' % (id, self.cstk)
-        #print(url)
         response = self.get(url)
         with open('%s/%s.docx' % (saveDir, id), 'w') as fp:
             fp.write(response.content)
@@ -88,8 +77,13 @@ def main(wf):
         notes = sess.search(sys.argv[1])
     i = 0
     while i < len(notes):
-        print(notes[i]['fileEntry']['name'])
-        wf.add_item(title=notes[i]['fileEntry']['name'])
+        note = notes[i]['fileEntry']
+        if note['name'][-3:] == '.md':
+            ntype = 'markdown'
+        else:
+            ntype = 'note'
+        url = "https://note.youdao.com/web/#/file/"+note['parentId']+'/'+ntype+'/'+note['id'] + '/?search=' + sys.argv[1]
+        wf.add_item(title=note['name'], valid=True, arg=url)
         i = i + 1
     wf.send_feedback()
 
